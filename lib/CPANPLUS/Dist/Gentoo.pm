7aa1972cf85390e3b6432fe63d1120dbb5967fdd
Vincent Pit <vince@profvince.com>
2009-01-25 01:26:56 +0100

    This is 0.05

--- lib/CPANPLUS/Dist/Gentoo.pm
+++ lib/CPANPLUS/Dist/Gentoo.pm
@@ -21,11 +21,11 @@ CPANPLUS::Dist::Gentoo - CPANPLUS backend generating Gentoo ebuilds.
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
